ef82567f7b82a9934ebab4d08235d35421f4e26a
Maria Villascusa <meri.villa14@gmail.com>
2022-02-07 16:42:36 +0100

    feat: create and show products

--- src/controllers/controller.py
+++ src/controllers/controller.py
@@ -1,6 +1,23 @@
+from flask import request, render_template, redirect
 from flask.views import MethodView
+from src.db import mysql
 
 class IndexController(MethodView):
     def get(self):
-        return 'BIENVENIDO'
+        with mysql.cursor() as cur:
+            cur.execute('SELECT * FROM products')
+            data = cur.fetchall()
+        return render_template('public/index.html', data=data)
+
+    def post(self):
+        code = request.form['code']
+        name = request.form['name']
+        stock = request.form['stock']
+        value = request.form['value']
+        category = request.form['category']
+
+        with mysql.cursor() as cur:
+            cur.execute("INSERT INTO products(code, name, stock, value) VALUES(%s, %s, %s, %s)",(code, name, stock, value))
+            cur.connection.commit()
+            return redirect('/')
 
